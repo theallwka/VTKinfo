99bd191d901fb7b9dcd51428ebf54688e73a4d34
theallwka <alidarberikuly@gmail.com>
2026-05-18 22:42:58 +0500

    Update script.js

--- script.js
+++ script.js
@@ -29,12 +29,12 @@ const data = {
 window.openModal = function(key) {
   const item = data[key];
 
-  document.getElementById("modaltitle").textContent = item.title;
-  document.getElementById("modalimg").src = item.img;
-  document.getElementById("modaldlitelnost").textContent = item.dlitelnost;
-  document.getElementById("modalbaza").textContent = item.baza;
-  document.getElementById("modalkod").textContent = item.kod;
-  document.getElementById("modalopisanie").textContent = item.opisanie;
+  document.getElementById("modalTitle").textContent = item.title;
+  document.getElementById("modalImg").src = item.img;
+  document.getElementById("modalDlitelnost").textContent = item.dlitelnost;
+  document.getElementById("modalBaza").textContent = item.baza;
+  document.getElementById("modalKod").textContent = item.kod;
+  document.getElementById("modalOpisanie").textContent = item.opisanie;
 
   document.getElementById("modal").style.display = "flex";
 };
